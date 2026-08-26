9823d7eaa8a3a24450b1be33482041f8af7b77c2
Ross Golder <ross@golder.org>
2026-08-26 17:03:42 +0700

    style: gofmt main.go

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -24,12 +24,12 @@ import (
 	"time"
 
 	"github.com/crossplane-contrib/provider-matrix/apis"
-	"github.com/crossplane-contrib/provider-matrix/apis/v1beta1"
 	powerlevelv1alpha1 "github.com/crossplane-contrib/provider-matrix/apis/powerlevel/v1alpha1"
 	roomv1alpha1 "github.com/crossplane-contrib/provider-matrix/apis/room/v1alpha1"
 	roomaliasv1alpha1 "github.com/crossplane-contrib/provider-matrix/apis/roomalias/v1alpha1"
 	spacev1alpha1 "github.com/crossplane-contrib/provider-matrix/apis/space/v1alpha1"
 	userv1alpha1 "github.com/crossplane-contrib/provider-matrix/apis/user/v1alpha1"
+	"github.com/crossplane-contrib/provider-matrix/apis/v1beta1"
 	"github.com/crossplane-contrib/provider-matrix/internal/controller"
 	"github.com/crossplane-contrib/provider-matrix/internal/features"
 	"github.com/crossplane-contrib/provider-matrix/internal/tracing"
